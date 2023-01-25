5615f7d818252a0177b03fed456087b60328f26f
Sahithi-2103 <119719345+Sahithi-2103@users.noreply.github.com>
2023-01-25 21:37:34 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<=i;j++)         {             printf("%c ",'A'-1 + i);         }         printf(" ");     }     return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,13 +3,14 @@ int main()
 {
     int n,i,j;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(i=n;i>=1;i--)
     {
         for(j=1;j<=i;j++)
         {
-            printf("%d",j);
+            printf("%c ",'A'-1 + i);
         }
         printf("
 ");
     }
+    return 0;
 }
